353d881c4bdd42cd3cfddc0a2e475bcb8095f93d
SUPRIYA MISHRA <140587863+supiimishra01@users.noreply.github.com>
2025-05-27 12:32:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
-
+  const strongEle = document.querySelectorAll('strong');
+  strongEle.forEach(function(e) {
+    e.style.color = 'rgb(0, 128, 0)';
+  });
+	
 }
 
 
 function return_normal() {
     //Write your code here
-
+const strongEle = document.querySelectorAll('strong');
+  strongEle.forEach(function(e) {
+    e.style.color = 'rgb(0, 0, 0)';
+  });
     
 }
